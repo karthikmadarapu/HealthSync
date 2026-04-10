6c31c9f691436b807bc631e2dcf4574e3daab41c
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-04-10 13:39:03 -0400

    fixed server port issue

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Auth Routes(Signup/ Login)
+// Auth Routes
 app.use("/api/auth", authRoutes);
 
-// Routes
+// Health Routes
 app.use("/api", healthRoutes);
 
 // Test route
 app.get("/", (req, res) => {
-  res.send(" Server Running");
+  res.send("Server Running");
 });
 
-const PORT = 5000;
+// ✅ ONLY ONE PORT + ONE LISTEN
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
